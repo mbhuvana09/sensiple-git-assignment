de3f40dfee73ae8e1a531cd3a4f90eb8a7e4697c
mbhuvana09 <143878748+mbhuvana09@users.noreply.github.com>
2024-11-02 12:03:44 +0530

    Update subtraction.py

--- subtraction.py
+++ subtraction.py
@@ -1,3 +1,7 @@
-def subtraction(a, b):
-    """Return the difference of two numbers."""
+""" # subtraction.py"""
+"""
+This module contains functions for subtraction operations.
+"""
+
+def subtract(a, b):
     return a - b
